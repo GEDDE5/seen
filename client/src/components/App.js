29eac22ba3a5e98b904c859ae4e4840fe768848a
GEDDE5 <geddes.3754@gmail.com>
2017-12-07 23:03:55 -0800

    Scaffolds create-react-app and configures base environment

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <p>SEEN</p>
+      </div>
+    )
+  }
+}
+
+export default App
