1acad3fb3ebd20d33c8bd885ee837c8526f46084
GEDDE5 <geddes.3754@gmail.com>
2017-12-08 00:04:46 -0800

    Writes generate_token def for use in application_controller current_user def

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
   has_many :permissions
   has_many :rooms, through: :permissions
   has_many :messages
+
+  def generate_token
+    rooms = self.rooms.pluck(:id)
+    Token.encode({ sub: self.id, rooms: rooms })
+  end
 end
