a13922859855142494001a8c08580dc790a411d3
GEDDE5 <geddes.3754@gmail.com>
2017-12-07 23:11:07 -0800

    Removes no-console disabling for eslint

--- server/middlewares/logger/index.js
+++ server/middlewares/logger/index.js
@@ -5,14 +5,13 @@ const logger = errMode => {
   if (!errMode) {
     return (req, res, next) => {
       const args = { req, res }
-      // eslint-disable-next-line no-console, no-underscore-dangle
+      // eslint-disable-next-line no-underscore-dangle
       console.log(reqInfo(args) + reqBody(req._body, req.body))
       next()
     }
   }
   return (err, req, res, next) => {
     const args = { err, req }
-    // eslint-disable-next-line no-console
     console.error(reqInfo(args))
     res.json({ error: err.message })
     next()
